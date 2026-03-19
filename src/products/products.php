939d297ba423acb6ca6f4e55f98411c90f448cb8
Wyatt Avilla <wyatt@linux.com>
2026-03-18 22:46:50 -0700

    dynamically build list of products

--- src/products/products.php
+++ src/products/products.php
@@ -1,22 +1,50 @@
+<?php
+function extractProductName($productPage) {
+    $contents = file_get_contents($productPage);
+
+    if ($contents === false) {
+        return null;
+    }
+
+    if (preg_match('/new\s+Product\s*\(\s*([\'"])(.*?)\1/s', $contents, $matches)) {
+        return $matches[2];
+    }
+
+    return null;
+}
+
+$productPages = glob(__DIR__ . '/*/*.php');
+$products = [];
+
+foreach ($productPages as $productPage) {
+    $relativePath = str_replace(__DIR__ . '/', '', $productPage);
+    $productName = extractProductName($productPage);
+
+    if ($productName === null) {
+        continue;
+    }
+
+    $products[] = [
+        'name' => $productName,
+        'path' => $relativePath,
+    ];
+}
+
+usort($products, function ($left, $right) {
+    return strcmp($left['name'], $right['name']);
+});
+?>
+
 <html>
 <title>Products</title>
 <h1>Products</h1>
-<table>
-  <tr>
-    <th>Name</th>
-    <th>Price</th>
-  </tr>
-  <tr>
-    <td>Shovel</td>
-    <td>$10.99</td>
-  </tr>
-  <tr>
-    <td>Plow</td>
-    <td>$3000.00</td>
-  </tr>
-  <tr>
-    <td>Snowboard</td>
-    <td>$150.00</td>
-  </tr>
-</table>
+<ul>
+<?php foreach ($products as $product): ?>
+  <li>
+    <a href="<?php echo htmlspecialchars($product['path'], ENT_QUOTES, 'UTF-8'); ?>">
+      <?php echo htmlspecialchars($product['name'], ENT_QUOTES, 'UTF-8'); ?>
+    </a>
+  </li>
+<?php endforeach; ?>
+</ul>
 </html>
