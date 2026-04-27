0ddeef0119beb869f7fa79a3b6f7718ce5ab2074
Wyatt Avilla <wyatt@linux.com>
2026-04-26 21:36:30 -0700

    user page w/ landing

--- src/home.php
+++ src/home.php
@@ -7,6 +7,7 @@
 		<li><a href="news.php">News</a></li>
 		<li><a href="contacts/contacts.php">Contacts</a></li>
 		<li><a href="users.php">Users</a></li>
+		<li><a href="user/user.php">User</a></li>
 		<li><a href="secure/login.php">Secure</a></li>
 		<li><a href="bio.php">Bio</a></li>
 		<li><a href="multiple_company_users.php">User From Multiple Companies</a></li>
---
+++ src/user/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User</title>
+</head>
+<body>
+	<h1>User</h1>
+	<ul>
+		<li><a href="create.php">Create User</a></li>
+		<li><a href="search.php">Search Users</a></li>
+	</ul>
+	<p><a href="../home.php">Back to Home</a></p>
+</body>
+</html>
